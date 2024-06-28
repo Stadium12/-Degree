d7a97f26dd52ae8e0bc160e05297024707fe7022
Stadium12 <nekitazkvskj@gmail.com>
2024-06-28 14:18:55 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+TOKEN="Токен"
+COGS_LIST=["meta", "welcome", "partnership", "yourcog", "slowmodecog","channelrestrictions" ,"clear", "blacklistcog", "yourcogname", "rolecog", "partnercog", "intocog", "helpcog", "avatar", "economy", "send", "functionssystem", "notfoundcog"] 
+
+Color=["3498db"]
+
+DEVS_ID=[1144376621399298099,788424538656210965,1101516349823529160,685502092118458387] 
+
+
+# С ЛЮБОВЬЮ ОТ trigand
